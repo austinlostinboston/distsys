5123c1c8ab280d369cc585863d8c7cc2d6aa2b63
Austin Ankney <codebeatstode@aol.com>
2015-07-17 11:44:45 -0400

    added memory monitoring

--- distsys/messages.py
+++ distsys/messages.py
@@ -1,4 +1,6 @@
 # Distsys.messages
+## Import builtins
+import resource
 
 def reader(msg, host, port):
     address = str(host) + ":" + str(port)
@@ -7,8 +9,10 @@ def reader(msg, host, port):
         rtn_msg = "Echo sent from " + address
     elif msg == "shutdown":
         rtn_msg = "Client " + address + " shutting down."
-    elif msg[0:2] == "add":
-        pass
+    elif msg == "mem_stat"
+        rtn_msg = "Client " + address + "\n" \
+        + "Service Memory Usage: " + str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (1024*1024.00)) + "MB" + "\n" \
+        + "Child Process Memory Usage: " + str(resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss / (1024*1024.00)) + "MB" + "\n"
     else:
         rtn_msg = "Command not understood by clients."
     return rtn_msg
