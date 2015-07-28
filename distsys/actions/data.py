1852e089438a80ecac78e9b114dbd18bef165a60
austinlostinboston <codebeatstode@aol.com>
2015-07-27 23:55:06 -0400

    fixed a few typos, data transfer now working

--- distsys/actions/data.py
+++ distsys/actions/data.py
@@ -3,6 +3,7 @@
 ## Import builtins
 import sys
 from multiprocessing import Pool 
+import os
 
 ## Import distsys
 from distsys.services import services
@@ -19,7 +20,10 @@ except:
     print "arg 2 - name for new directory"
     sys.exit()
 
-def transfer_data(ip_addr, files):
+def transfer_data(combo):
+    ip_addr = combo[0]
+    files = combo[1]
+
     ## Make project folder for data
     mkdir(ip_addr,'~/distsys/data/', project)
 
@@ -36,9 +40,9 @@ if __name__ == "__main__":
 
     client_files = distribute_data(num_clients, path)
     combo = []
-    for i in range(s.clients):
+    for i in range(num_clients):
         combo.append([s.clients[i],client_files[i]])
 
     print "Distributing data..."
     pool = Pool(processes=num_clients)
-    pool.map(client_status, combo)
+    pool.map(transfer_data, combo)
